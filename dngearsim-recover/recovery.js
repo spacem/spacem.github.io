0692ae6b699d540e77e187c79948547c96d50ada
spacemh <spaceyspacespace@gmail.com>
2016-12-07 06:14:42 +0000

    change build list

---
+++ dngearsim-recover/recovery.js
@@ -0,0 +1,30 @@
+angular.module('dnsim').controller('recoveryCtrl',
+
+['saveHelper',
+function(saveHelper) {
+  'use strict';
+  
+  var vm = this;
+    
+    this.setupBuilds = function() {
+      vm.savedItems = saveHelper.getSavedItems();
+      vm.buildNames = Object.keys(vm.savedItems).sort();
+      vm.builds = [];
+      for(var i=0;i<vm.buildNames.length;++i) {
+        vm.builds.push({
+          name: vm.buildNames[i],
+          build: vm.savedItems[vm.buildNames[i]],
+        });
+      }
+    }
+    this.setupBuilds();
+  
+}])
+.directive('dngearsimBuildItem', function() {
+  return {
+    scope: {},
+    controller: 'recoveryCtrl',
+    controllerAs: 'ctrl',
+    templateUrl: '/dngearsim-recover/recovery.html'
+  };
+});
